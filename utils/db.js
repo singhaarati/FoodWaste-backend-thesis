2c178a08738102ea37d7164f6d0b5d4f3551f135
singhaarati <aarati5737@gmail.com>
2023-12-06 16:47:37 +0545

    user login and signup apis done

---
+++ utils/db.js
@@ -0,0 +1,19 @@
+// utils/db.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true, // Corrected option name
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
